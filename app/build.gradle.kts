036e2f3ff232d77eb1570cbe1eedd08e74bc8a18
tux4us <tux4us@online.de>
2026-01-11 19:58:44 +0100

    VollaHub_2.2 Colour optimisation for better readability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.volla.hub"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
     }
 
     buildTypes {
